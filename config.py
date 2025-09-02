183daedd24eb27b680184e6a4f7d5c654d2f3ecd
ASUS <luckyomer10@gmail.com>
2025-09-02 15:45:10 +0530

    index

--- config.py
+++ config.py
@@ -9,5 +9,9 @@ MONGO_URI = os.getenv("MONGO_URI")
 JWT_SECRET = os.getenv("JWT_SECRET")
 DB_NAME=os.getenv("DB_NAME")
 JWT_EXPIRE_MIN = int(os.getenv("JWT_EXPIRE_MIN", "60"))
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
 db = client[DB_NAME]
